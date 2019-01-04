b429a3ac0d3efc41a69635c63e3e30b06d2e1725
William Saffran <willh.saffran@gmail.com>
2019-01-04 23:40:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
+
 var playlist = new Object({Masego: 'Sunday Vibes'})
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, playlist, {[artistName]: songTitle})
+  return playlist
+}
