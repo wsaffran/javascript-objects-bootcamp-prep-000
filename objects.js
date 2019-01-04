be3f64c499cd3a297a926f442a771559cc36e219
William Saffran <willh.saffran@gmail.com>
2019-01-04 23:47:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+function removeFromPlaylist(object, artistName) {
+  delete object.artistName
   return playlist
 }
